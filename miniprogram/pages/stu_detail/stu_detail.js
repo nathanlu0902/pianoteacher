3f24cea0889f33ceff8743d1fc62418c4417673f
NL <nathan.lu0902@outlook.com>
2020-11-22 12:45:44 +0800

    new

--- miniprogram/pages/stu_detail/stu_detail.js
+++ miniprogram/pages/stu_detail/stu_detail.js
@@ -1,5 +1,19 @@
 Page({
   data:{
+    student_list: []
+  },
+  onLoad: function () {
+    let that=this
+    const db=wx.cloud.database()
+    const student=db.collection("student")
+    student.where({}).get({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          student_list: res.data
+        })
+      }
+    })
 
   },
   add_stu: function() {
